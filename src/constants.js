52dd48b57f5002a0bd684fc1d4f09d1e30e26da6
mattmoras-musictuition <mattmoras@gmail.com>
2026-05-29 07:55:16 +1000

    Bump version to 2.9.11

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const INSTRUMENTS = [
   "Ukulele", "Recorder", "Bass Guitar"
 ];
 
-export const APP_VERSION = "2.9.10";
+export const APP_VERSION = "2.9.11";
 export const HEADER_HEIGHT = 90; // Height of page banners and logo box
 export const TIMEZONE = "Australia/Melbourne";
 
